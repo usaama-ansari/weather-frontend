4114791720f82c6479967cdbe8474aef676d16e6
usaama ansari <usaama.ansari@massiveanalytic.com>
2023-03-26 01:35:20 +0530

    feat(services): implement city service

--- src/services/city/index.ts
+++ src/services/city/index.ts
@@ -1,1 +1,31 @@
+import { inject, injectable } from "inversify";
+import { TYPES_IOC } from "@Helpers/ioc/constants";
+import type { IRestClient, RequestError } from "@Helpers/network";
+import { Result } from "@Helpers/Result";
+import { ICityService } from "./ICityService";
+
+@injectable()
+export class CityService implements ICityService {
+  @inject(TYPES_IOC.RestClient)
+  private restClient: IRestClient;
+
+  async addCity(cityName: string) {
+    try {
+      await this.restClient.post("efw", { city: cityName });
+      return Result.ok<void>();
+    } catch (err) {
+      return Result.fail(err as RequestError);
+    }
+  }
+
+  async addCities(cityNames: string[]) {
+    try {
+      await this.restClient.post("efw", { cities: cityNames });
+      return Result.ok<void>();
+    } catch (err) {
+      return Result.fail(err as RequestError);
+    }
+  }
+}
+
 export * from "./ICityService";
