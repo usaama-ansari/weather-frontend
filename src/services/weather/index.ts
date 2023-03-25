8ff5ec9510b9ba668f5fde153328a6b02eff9549
usaama ansari <usaama.ansari@massiveanalytic.com>
2023-03-26 02:02:25 +0530

    feat(services): implement weather service

--- src/services/weather/index.ts
+++ src/services/weather/index.ts
@@ -1,1 +1,30 @@
+/* eslint-disable */
+import { inject, injectable } from "inversify";
+import { TYPES_IOC } from "@Helpers/ioc/constants";
+import type { IRestClient, RequestError } from "@Helpers/network";
+import { Result } from "@Helpers/Result";
+import { IWeatherService } from "./IWeatherService";
+import { CityWeatherDTO } from "@Types";
+
+@injectable()
+export class WeatherService implements IWeatherService {
+  @inject(TYPES_IOC.RestClient)
+  private restClient: IRestClient;
+
+  async fetchCityCurrentWeather(cityName: string) {
+    try {
+      const request = await this.restClient.get("efw", {
+        params: {
+          city: cityName,
+        },
+      });
+      // TODO: use data mapper for weather data
+      const data: CityWeatherDTO = request.data;
+      return Result.ok(data);
+    } catch (err) {
+      return Result.fail(err as RequestError);
+    }
+  }
+}
+
 export * from "./IWeatherService";
